e31a56ff6e914005857607dd0108d74ef9370f53
rikeshbohara <rikesh.bohara@bajratechnologies.com>
2025-10-19 12:32:52 +0000

    feat: add default false for profile complete

--- db/migrate/20251017092101_add_profile_completed_to_users.rb
+++ db/migrate/20251017092101_add_profile_completed_to_users.rb
@@ -1,5 +1,5 @@
 class AddProfileCompletedToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :profile_completed, :boolean
+    add_column :users, :profile_completed, :boolean, default: false, null: false
   end
 end
